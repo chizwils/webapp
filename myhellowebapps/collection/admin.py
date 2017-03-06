81324e1d86c19650ce0a99d26036013db6506216
chinedu chizaram <c.chizaram92@gmail.com>
2017-03-06 11:02:02 -0600

    setup dtabase

--- myhellowebapps/collection/admin.py
+++ myhellowebapps/collection/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+#import the model first
+from collection.models import Thing
+
+#setup automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+	model = Thing
+	#we want the name and description field to show up in the admin
+	list_diplay = ('name', 'description')
+	prepopulated_fields ={'slug': ('name',)}
+
+# and then register it 
+admin.site.register(Thing, ThingAdmin)
